927e514417eab284278908ba6e7c5e9ad0efa225
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-04-19 12:06:41 +0200

    Write getter

--- src/main/java/Model/Publisher.java
+++ src/main/java/Model/Publisher.java
@@ -2,6 +2,11 @@ package Model;
 
 public class Publisher {
     private String ID;
+
+    public String getName() {
+        return name;
+    }
+
     private String name;
 
     public Publisher(String name) {
