f5b6f9ee51cf6669f6da0503bdc6d26bf16d8db2
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-04-19 12:06:59 +0200

    Write print TreeMap

--- src/main/java/View/AbstractOutput.java
+++ src/main/java/View/AbstractOutput.java
@@ -1,5 +1,11 @@
 package View;
 
+import Model.Book;
+
+import java.util.Map;
+
 public abstract class AbstractOutput {
     public abstract void print(String s);
+
+    public abstract void print(Map<String, Book> books);
 }
--- src/main/java/View/OutputManager.java
+++ src/main/java/View/OutputManager.java
@@ -1,8 +1,19 @@
 package View;
 
+import Model.Book;
+
+import java.util.Map;
+
 public class OutputManager extends AbstractOutput{
     @Override
     public void print(String s) {
         System.out.println(s);
+    }
+
+    @Override
+    public void print(Map<String, Book> books) {
+        for (Map.Entry<String,Book> book: books.entrySet() ) {
+            System.out.println(book.getValue().toString());
+        }
     }
 }
