0f4b3ba0f551c76e70fd189b1cafce07b8be631b
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-04-19 10:40:57 +0200

    Add fields to Controller

--- src/main/java/Controller/ControllerLibrary.java
+++ src/main/java/Controller/ControllerLibrary.java
@@ -5,7 +5,17 @@ import View.AbstractInput;
 import View.AbstractOutput;
 
 public class ControllerLibrary {
+    AbstractOutput output;
+    AbstractInput input;
+    BookDBDAO dao;
+
     public ControllerLibrary(AbstractOutput output, AbstractInput input, BookDBDAO bookDBDAO) {
+        this.output = output;
+        this.input = input;
+        this.dao = bookDBDAO;
+    }
+
+    public void run(){
 
     }
 }
