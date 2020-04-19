c153615ced483cea0d07046785ef9a707ba019be
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-04-19 10:57:19 +0200

    Write Interface DAO

--- src/main/java/DAO/IDAOBook.java
+++ src/main/java/DAO/IDAOBook.java
@@ -1,4 +1,14 @@
 package DAO;
 
-public class IDAOBook {
+import Model.Book;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IDAOBook {
+    void addBook();
+    void editBook();
+    void deleteBook();
+    List<Book> searchBooks();
+    Map<String,Book> getBooks();
 }
