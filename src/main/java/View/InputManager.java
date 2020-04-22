a1c4de7ebe36a2187c78cb2b3f5d555039c39dd0
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-04-23 00:50:11 +0200

    Repair literals

--- src/main/java/View/InputManager.java
+++ src/main/java/View/InputManager.java
@@ -39,7 +39,7 @@ public class InputManager extends AbstractInput {
         Scanner scanFromUser = new Scanner(System.in);
 
         while(!scanFromUser.hasNextLong()){
-            output.print("Wrong input! Please insert the integer number.");
+            output.print("Wrong input! Please insert the long number.");
             scanFromUser.next();
         }
         input = scanFromUser.nextLong();
@@ -53,7 +53,7 @@ public class InputManager extends AbstractInput {
         Scanner scanFromUser = new Scanner(System.in);
 
         while(!scanFromUser.hasNextFloat()){
-            output.print("Wrong input! Please insert the integer number.");
+            output.print("Wrong input! Please insert the float number.");
             scanFromUser.next();
         }
         input = scanFromUser.nextFloat();
